6b30639fe8bb5243719c90cc987f9c8e4e6fb260
Rémi Domingues <remi.domingues@insa-lyon.fr>
2014-03-10 11:58:03 +0100

    MeanShift refactoring

--- Projet/python/plot.py
+++ Projet/python/plot.py
@@ -3,10 +3,12 @@ import pylab as pl
 from itertools import cycle
 
 def draw_meanshift(data, meanshift):
+    data['cluster'] = meanshift.labels_
     labels = meanshift.labels_
     cluster_centers = meanshift.cluster_centers_
 
     labels_unique = np.unique(labels)
+    print labels_unique
     n_clusters_ = len(labels_unique)
     n_centers_ = len(cluster_centers)
 
@@ -16,6 +18,7 @@ def draw_meanshift(data, meanshift):
     colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
 
     for k, col in zip(range(n_clusters_), colors):
+
         my_members = labels == k
         if k < n_centers_ and len(data[my_members]) > 50:
             print len(data[my_members, 0])
