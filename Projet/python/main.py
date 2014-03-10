6ecc5defbdd6f8f4ce6aa5ec61e2dc34cc47e9fa
Rémi Domingues <remi.domingues@insa-lyon.fr>
2014-03-10 08:11:18 +0100

    Moved Knime folder to other directory

--- Projet/python/main.py
+++ Projet/python/main.py
@@ -1,11 +1,52 @@
-from parser import Parser
+import parser
+import plot
 
-"""
-TODO:
-- Call DBSCAN
-- Display results
-=> See examples !
-"""
+from sklearn.cluster import MeanShift, estimate_bandwidth
+from sklearn.datasets.samples_generator import make_blobs
+
+def test():
+    #Data
+    centers = [[1, 1], [-1, -1], [1, -1]]
+    X, _ = make_blobs(n_samples=10000, centers=centers, cluster_std=0.6)
+
+    # Compute clustering with MeanShift
+    # Parameters: data, quantile=radius max of the cluster, n_samples=minimum points per cluster
+    bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=500)
+
+    ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+    ms.fit(X)
+
+    plot.draw_meanshift(X, ms)
+
+
+def run_meanshift(data):
+    print data[0]
+    # Compute clustering with MeanShift
+    # Parameters: data, quantile=radius max of the cluster, n_samples=minimum points per cluster
+    
+    #TODO; data class must be ndarray (cf numpy) => see other examples
+    bandwidth = estimate_bandwidth(data, quantile=0.002, n_samples=30)
+    ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+    ms.fit(data)
+
+    return ms
+
+
+def get_ms_data(headers, data):
+    ms_data = []
+    lat_idx = headers['latitude']
+    lon_idx = headers['longitude']
+
+    for row in data:
+        ms_data.append([row[lat_idx], row[lon_idx]])
+
+    return ms_data
+
 
 if __name__ == "__main__":
-    headers, data = Parser.parse_csv("data.csv")
+    test()
+
+    headers, data = parser.parse_csv(filepath="data.csv", limit=5000)
+    ms_data = get_ms_data(headers=headers, data=data)
+    ms = run_meanshift(data=ms_data)
+    plot.draw_meanshift(data=ms_data, meanshift=ms)
--- Projet/python/parser.py
+++ Projet/python/parser.py
@@ -1,30 +1,31 @@
 import csv, sys
 
-class Parser(object):
-    @staticmethod
-    def parse_csv(filepath):
-        print "Parsing file {}...".format(filepath)
-        
-        headersDict = dict()
-        data = []
+def parse_csv(filepath, limit=-1):
+    print "Parsing file {}...".format(filepath)
+    
+    headersDict = dict()
+    data = []
 
-        with open(filepath, 'rb') as f:
-            reader = csv.reader(f)
-            try:
-                #Building headers indexes
-                headers = reader.next()
+    with open(filepath, 'rb') as f:
+        reader = csv.reader(f)
+        try:
+            #Building headers indexes
+            headers = reader.next()
 
-                for i in range(0, len(headers)):
-                    headersDict[headers[i]] = i
+            for i in range(0, len(headers)):
+                headersDict[headers[i]] = i
 
-                #Parsing data
-                i = 0
-                for row in reader:
-                    data.append(row)
+            #Parsing data
+            i = 0
+            for row in reader:
+                if i == limit:
+                    break 
+                data.append(row)
+                i += 1
 
-            except csv.Error as e:
-                sys.exit('File %s, line %d: %s' % (filename, reader.line_num, e))
+        except csv.Error as e:
+            sys.exit('File %s, line %d: %s' % (filename, reader.line_num, e))
 
-        print "Done ({} rows read)".format(len(data))
+    print "Done ({} rows read)".format(len(data))
 
-        return headersDict, data
+    return headersDict, data
