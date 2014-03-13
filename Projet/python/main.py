58482109bfdab8507bc5d6c9f79afcbf7840706d
Rémi Domingues <remi.domingues@insa-lyon.fr>
2014-03-13 13:30:47 +0100

    Data filtering for meanshift + csv export

--- Projet/python/main.py
+++ Projet/python/main.py
@@ -9,58 +9,84 @@ import numpy as np
 import pylab as pl
 from itertools import cycle
 
+def import_data(filepath):
+    df = pandas.read_csv(filepath_or_buffer=filepath)  # Read the file
+    return df[['longitude', 'latitude', 'hashtags']]
 
 def run_meanshift(data):
     # Compute clustering with MeanShift
     bandwidth = estimate_bandwidth(data, quantile=0.002, n_samples=1000)
     ms = MeanShift(bandwidth=bandwidth, bin_seeding=True, min_bin_freq=30, cluster_all=False)
     ms.fit(data)
-
     return ms
 
-if __name__ == "__main__":
-    #test()
-
-    df = pandas.read_csv(filepath_or_buffer="data.csv")  # Read the file
-    ms_data = df[['longitude', 'latitude']]
-    ms_values = ms_data.values
-
-    meanshift = run_meanshift(data=ms_values)
-    #plot.draw_meanshift(data=ms_data, meanshift=ms)
-
-
-
-
-    ms_data['cluster'] = meanshift.labels_
-    ms_data = ms_data.sort(columns='cluster')
-
-    cluster_centers = meanshift.cluster_centers_
-    labels_unique = np.unique(meanshift.labels_)
-
-    n_centers_ = len(cluster_centers)
-    n_clusters_ = len(labels_unique)
-
-    print("Number of estimated clusters: %d" % n_clusters_)
-
+def plot_meanshift(ms_data, cluster_centers, n_centers_):
     colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
     pl.figure(1)
     pl.clf()
 
-    for k, col in zip(range(n_clusters_), colors):
-        if k != -1 and k < n_centers_:
-            cluster_points = ms_data[(ms_data.cluster == k)]
+    for k, col in zip(cluster_centers['cluster'], colors):
+        cluster_points = ms_data[(ms_data['cluster'] == k)]
+        #cluster_points = ms_data[(ms_data.cluster == k)]
 
-            print len(cluster_points)
+        cluster_longitudes = cluster_points['longitude']
+        cluster_latitudes = cluster_points['latitude']
 
-            if len(cluster_points) > 30:
-                cluster_longitudes = cluster_points['longitude']
-                cluster_latitudes = cluster_points['latitude']
-        
-                cluster_center = cluster_centers[k]
+        cluster_center = cluster_centers[(cluster_centers['cluster'] == k)]
 
-                pl.plot(cluster_longitudes, cluster_latitudes, col + '.')
-                pl.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
-                    markeredgecolor='k', markersize=14)
+        pl.plot(cluster_longitudes, cluster_latitudes, col + '.')
+        pl.plot(cluster_center['longitude'], cluster_center['latitude'], 'o', markerfacecolor=col,
+            markeredgecolor='k', markersize=14)
 
-    pl.title('POI clustering (%d estimated clusters)' % n_clusters_)
+    pl.title('POI clustering (%d estimated clusters)' % n_centers_)
     pl.show()
+
+def export_data(ms_data, clusters_file, cluster_centers, centers_file):
+    ms_data.to_csv(path_or_buf=clusters_file, cols=['longitude', 'latitude', 'hashtags', 'cluster'], encoding='utf-8')
+    cluster_centers.to_csv(path_or_buf=centers_file, cols=['longitude', 'latitude', 'cluster'], encoding='utf-8')
+
+if __name__ == "__main__":
+    ms_data = import_data("data/data.csv")
+    ms_values = ms_data.ix[:, 0:2].values
+
+    meanshift = run_meanshift(data=ms_values)
+    ms_data['cluster'] = meanshift.labels_
+
+    #Filtering data
+    ms_data = ms_data[(ms_data['longitude'] < 4.908976) & (ms_data['longitude'] > 4.802508)
+        & (ms_data['latitude'] < 45.793688) & (ms_data['latitude'] > 45.715569)]
+
+    ms_data = ms_data.sort(columns='cluster')
+
+    # Removing points which belong to no cluster
+    ms_data = ms_data[(ms_data['cluster'] != -1)]
+
+
+    labels_unique = np.unique(meanshift.labels_).tolist()
+    del labels_unique[0]
+
+    #Filetering clusters centers according to data filter
+    cluster_centers = DataFrame(meanshift.cluster_centers_, columns=['longitude', 'latitude'])
+    cluster_centers['cluster'] = labels_unique
+    cluster_centers = cluster_centers[(cluster_centers['longitude'] < 4.908976) & (cluster_centers['longitude'] > 4.802508)
+        & (cluster_centers['latitude'] < 45.793688) & (cluster_centers['latitude'] > 45.715569)]
+    n_centers_ = len(cluster_centers)
+
+
+    # Removing clusters which contain less than x pointsnext(df.iterrows())[1]
+    for k in range(n_centers_):
+        try:
+            cluster_points = ms_data[(ms_data['cluster'] == k)]
+            if len(cluster_points) < 30:
+                print "Removing cluster {} ({} points)".format(k, len(cluster_points))
+                ms_data = ms_data[(ms_data['cluster'] != k)]
+                cluster_centers = cluster_centers[(cluster_centers['cluster'] != k)]
+        except:
+            print "Removing cluster {} (no points)".format(k)
+            cluster_centers = cluster_centers[(cluster_centers['cluster'] != k)]
+
+    print("Number of estimated clusters: %d" % n_centers_)
+
+    #plot_meanshift(ms_data, cluster_centers, n_centers_)
+
+    export_data(ms_data, "results/clusters.csv", cluster_centers, "results/centers.csv")
