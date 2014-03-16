fd5faefacc5c95cfc3d10ec9de98ecb74ff36dcf
golgoth <adrien.duffy@gmail.com>
2014-03-16 22:18:12 +0100

    clusters and points ok, still need to show pictures and data onclick

--- Projet/python/server.py
+++ Projet/python/server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import json
 import pandas
 import math
+import random
 
 app = Flask(__name__)
 app.debug = True
@@ -29,22 +30,20 @@ def generatePoints():
     if request.method == 'GET':
         points = pandas.read_csv(filepath_or_buffer=filepathPoints)  # Read the file
 
-        table_points = points[['longitude', 'latitude','hashtags' ,'cluster' ]].values.tolist()
-
+        table_points = points[['longitude', 'latitude','hashtags','url' ,'cluster' ]]
+        table_points = table_points.fillna('').values.tolist()
 
         points = pandas.read_csv(filepath_or_buffer=filepathClusters)  # Read the file
-        table_clusters = points[['longitude','latitude', 'cluster']].values.tolist()
+        table_clusters = points[['longitude','latitude', 'cluster']].fillna('').values.tolist()
 
 
         # now creating a list of objects in this form : [cluster_number, center point] , [list of the points contained in this cluster]
-        result =[ [ [cluster,table_clusters[(table_clusters[2] == cluster)],[] ], [l for l in table_points if l[3] == cluster]  ] for cluster in set([ p[3] for p in table_points])]
+        result =[ [ [cluster,[clust for clust in table_clusters if clust[2] == cluster][0],10 ], [l for l in table_points if l[4] == cluster]  ] for cluster in set([ p[4] for p in table_points])]
         
         for cluster in result :
-            cluster[0][2] = max_cluster(cluster[1],cluster[0][1])
+            for point in cluster[1] :
+                point[1] = point[1]+0.0005-(random.randint(0,10)/10000.0)#max_cluster(cluster[1],cluster[0][1])
 
-        return json.dumps(result)
-
-        result =[ [ [cluster,table_clusters[table_clusters[2] == cluster] ], [l for l in table_points if l[3] == cluster]  ] for cluster in set([ p[3] for p in table_points])]
         return json.dumps(result)
 
 if __name__ == '__main__':
